287c7d383fd54588135d69a836954c15d250ed36
Ali  Aryaeifar <aryaeifar12@gmail.com>
2024-03-28 14:45:09 +0430

    add minagoo project

---
+++ components/Modal/BaseModal.vue
@@ -0,0 +1,60 @@
+<script setup>
+import { Carousel, Slide, Navigation, Pagination } from "vue3-carousel";
+import "vue3-carousel/dist/carousel.css";
+const props = defineProps({
+  value: {
+    type: Boolean,
+  },
+  item: {
+    type: [Array, Object],
+  },
+});
+
+const currentSlide = ref(0);
+const slideTo = (val) => {
+  currentSlide.value = val;
+};
+const settings = {
+  dir: "rtl",
+  snapAlign: "center",
+};
+
+const activeDesc = ref(true);
+</script>
+
+<template>
+  <v-dialog v-model="dialog" class="base-modal">
+    <v-card color="dark" class="base-modal-card rounded-xl pa-5"> 
+      <div class="">
+        <Carousel :items-to-show="2" :wrap-around="false">
+          <Slide v-for="slide in item" :key="slide">
+               <v-img aspect-ratio="4/3" class="rounded-xl" :src="'/assets/images/' + slide.img" cover></v-img>
+          </Slide>
+          <template #addons>
+            <Navigation />
+            <Pagination />
+          </template>
+        </Carousel>
+      </div>
+
+    </v-card>
+  </v-dialog>
+</template>
+<style scoped>
+.carousel__item {
+  width: 100%;
+  padding: 0 0.3rem;
+}
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
+}
+.carousel__slide {
+  padding: 10px;
+}
+</style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -5,6 +5,18 @@ useHead({
     { name: 'description', content: 'My amazing site.' },
   ],
 })
+const minagoo = ref(false)
+const flylounge = ref(false)
+
+const selectedProject = [
+  {name:'Site1', img:"minagoo-thumbnail.png"}, 
+  {name:'site2', img:"minagoo-explore.png"},
+  {name:'site3', img:"minagoo-lessons.png"},
+  {name:'site4', img:"minagoo-teachers.png"},
+  {name:'site4', img:"minagoo-package.png"},
+    {name:'site4', img:"minagoo-exercise.png"},
+
+]
 </script>
 
 <template>
@@ -16,151 +28,111 @@ useHead({
       <div class="">
         <v-container>
           <v-row>
-            <v-col cols="12" lg="3" style="--stagger: 1" data-animate>
-              <v-card class="mx-auto projects-card" max-width="374">
+            <v-col cols="12" lg="4" style="--stagger: 1" data-animate>
+              <div type="button" @click="minagoo = true">
+                <v-card class="mx-auto projects-card" max-width="374">
                 <template v-slot:loader="{ isActive }">
-                  <v-progress-linear
+                  <v-progress-linear  
                     :active="isActive"
                     color="deep-purple"
                     height="4"
                     indeterminate></v-progress-linear>
                 </template>
 
-                <v-img cover height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+                <v-img cover height="250" src="/assets/images/minagoo-thumbnail.png"></v-img>
 
                 <v-card-item>
-                  <v-card-title>Cafe Badilico</v-card-title>
+                  <v-card-title>Minagoo</v-card-title>
                 </v-card-item>
 
                 <v-card-text>
                   <div>
-                    Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.
+                    Minagoo is an online English learning platform to Improve your language skills and it can be used for any age, level, and nationality.
+                    <p>
+                      Front-end technologies:
+                    </p>
+                    <ul class="mx-3 my-3">
+                      <li style="font-size: 12px;">
+                        HTML-CSS
+                      </li>
+                      <li style="font-size: 12px;">
+                        JavaScript
+                      </li>
+                      <li style="font-size: 12px;">
+                        VUE
+                      </li>
+                      <li style="font-size: 12px;">
+                        NUXT
+                      </li>
+                    </ul>
                   </div>
                 </v-card-text>
                 <v-card-item>
                   <v-card-subtitle>
+                   <div>
                     <span class="me-1">Des 6, 2023</span>
+                   </div>
                   </v-card-subtitle>
                 </v-card-item>
               </v-card>
+              </div>
+              
             </v-col>  
-            <v-col cols="12" lg="3" style="--stagger: 1" data-animate>
-              <v-card class="mx-auto projects-card" max-width="374">
+              <v-col cols="12" lg="4" style="--stagger: 1" data-animate>
+              <div type="button" @click="flylounge = true">
+                <v-card class="mx-auto projects-card" max-width="374">
                 <template v-slot:loader="{ isActive }">
-                  <v-progress-linear
+                  <v-progress-linear  
                     :active="isActive"
                     color="deep-purple"
                     height="4"
                     indeterminate></v-progress-linear>
                 </template>
 
-                <v-img cover height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
+                <v-img cover height="250" src="/assets/images/minagoo-thumbnail.png"></v-img>
 
                 <v-card-item>
-                  <v-card-title>Cafe Badilico</v-card-title>
+                  <v-card-title>Flylounge</v-card-title>
                 </v-card-item>
 
                 <v-card-text>
                   <div>
-                    Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.
+                    Minagoo is an online English learning platform to Improve your language skills and it can be used for any age, level, and nationality.
+                    <p>
+                      Front-end technologies:
+                    </p>
+                    <ul class="mx-3 my-3">
+                      <li style="font-size: 12px;">
+                        HTML-CSS
+                      </li>
+                      <li style="font-size: 12px;">
+                        JavaScript
+                      </li>
+                      <li style="font-size: 12px;">
+                        VUE
+                      </li>
+                      <li style="font-size: 12px;">
+                        NUXT
+                      </li>
+                    </ul>
                   </div>
                 </v-card-text>
                 <v-card-item>
                   <v-card-subtitle>
+                   <div>
                     <span class="me-1">Des 6, 2023</span>
+                   </div>
                   </v-card-subtitle>
                 </v-card-item>
               </v-card>
-            </v-col>
-            <v-col cols="12" lg="3" style="--stagger: 1" data-animate>
-              <v-card class="mx-auto  projects-card" max-width="374">
-                <template v-slot:loader="{ isActive }">
-                  <v-progress-linear
-                    :active="isActive"
-                    color="deep-purple"
-                    height="4"
-                    indeterminate></v-progress-linear>
-                </template>
-
-                <v-img cover height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
-
-                <v-card-item>
-                  <v-card-title>Cafe Badilico</v-card-title>
-                </v-card-item>
-
-                <v-card-text>
-                  <div>
-                    Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.
-                  </div>
-                </v-card-text>
-                <v-card-item>
-                  <v-card-subtitle>
-                    <span class="me-1">Des 6, 2023</span>
-                  </v-card-subtitle>
-                </v-card-item>
-              </v-card>
-            </v-col>
-            <v-col cols="12" lg="3" style="--stagger: 1" data-animate>
-              <v-card class="mx-auto  projects-card" max-width="374">
-                <template v-slot:loader="{ isActive }">
-                  <v-progress-linear
-                    :active="isActive"
-                    color="deep-purple"
-                    height="4"
-                    indeterminate></v-progress-linear>
-                </template>
-
-                <v-img cover height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
-
-                <v-card-item>
-                  <v-card-title>Cafe Badilico</v-card-title>
-                </v-card-item>
-
-                <v-card-text>
-                  <div>
-                    Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.
-                  </div>
-                </v-card-text>
-                <v-card-item>
-                  <v-card-subtitle>
-                    <span class="me-1">Des 6, 2023</span>
-                  </v-card-subtitle>
-                </v-card-item>
-              </v-card>
-            </v-col>
-            <v-col cols="12" lg="3" style="--stagger: 2" data-animate>
-              <v-card class="mx-auto projects-card" max-width="374">
-                <template v-slot:loader="{ isActive }">
-                  <v-progress-linear
-                    :active="isActive"
-                    color="deep-purple"
-                    height="4"
-                    indeterminate></v-progress-linear>
-                </template>
-
-                <v-img cover height="250" src="https://cdn.vuetifyjs.com/images/cards/cooking.png"></v-img>
-
-                <v-card-item>
-                  <v-card-title>Cafe Badilico</v-card-title>
-                </v-card-item>
-
-                <v-card-text>
-                  <div>
-                    Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.
-                  </div>
-                </v-card-text>
-                <v-card-item>
-                  <v-card-subtitle>
-                    <span class="me-1">Des 6, 2023</span>
-                  </v-card-subtitle>
-                </v-card-item>
-              </v-card>
-            </v-col>
-            <v-col cols="12" lg="3"> 1 </v-col>
-            <v-col cols="12" lg="3"> 1 </v-col>
+              </div>
+              
+            </v-col> 
           </v-row>
         </v-container>
       </div>
     </article>
+    <ModalBaseModal v-model="minagoo" :item="selectedProject" />
+
   </div>
 </template>
