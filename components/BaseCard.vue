8068021d01bb044aca6a89968c436061806b82bc
Ali  Aryaeifar <aryaeifar12@gmail.com>
2024-07-20 20:27:14 +0330

    add projects

---
+++ components/BaseCard.vue
@@ -0,0 +1,49 @@
+<script setup>
+const props = defineProps({
+    items:{
+        type:Object
+    }
+})
+</script>
+
+<template>
+  <div class="h-100">
+    <v-card class="mx-auto projects-card h-100" max-width="374">
+      <v-img
+        cover
+        height="250"
+        :src="'/assets/images/' + items.img"
+        class="ma-3 rounded basecard-img"
+      ></v-img>
+
+      <v-card-item>
+        <v-card-title>{{items.title}}</v-card-title>
+      </v-card-item>
+
+      <v-card-text>
+        <div>
+        {{ items.text }}
+          <p class="mt-3">Front-end technologies:</p>
+          <ul class="mx-3 my-3">
+            <li style="font-size: 12px" v-for="i in items.frontTech">
+                {{ i }}
+            </li>
+          </ul>
+        </div>
+        <div>
+          <a :href="items.link" target="_blank">{{items.linkName}}</a>
+        </div>
+      </v-card-text>
+      <v-card-item>
+        <v-card-subtitle>
+          <div>
+            <span class="me-1">{{ items.date }}</span>
+          </div>
+        </v-card-subtitle>
+      </v-card-item>
+    </v-card>
+  </div>
+</template>
+
+
+<style lang="scss" scoped></style>
--- pages/projects.vue
+++ pages/projects.vue
@@ -1,31 +1,55 @@
 <script lang="ts" setup>
 useHead({
-  title: 'Projects - Ali arya',
-  meta: [
-    { name: 'description', content: 'My amazing site.' },
-  ],
-})
-const minagoo = ref(false)
-const flylounge = ref(false)
-const shahrdari = ref(false)
-
-const minagooInfo = [
-  {name:'Site1', img:"minagoo-thumbnail.png"}, 
-  {name:'site2', img:"minagoo-explore.png"},
-  {name:'site3', img:"minagoo-lessons.png"},
-  {name:'site4', img:"minagoo-teachers.png"},
-  {name:'site4', img:"minagoo-package.png"},
-  {name:'site4', img:"minagoo-exercise.png"},
-]
-const flyInfo = [
-  {name:'Site1', img:"flylounge-menu.png"}, 
-]
-const shahrdariInfo = [
-  {name:'Site1', img:"shahrdari-main.png"}, 
-  {name:'Site2', img:"shahrdari-peygiri.png"}, 
-{name:'Site2', img:"shahrdari-sabt.png"}, 
-]
-
+  title: "Projects - Ali arya",
+  meta: [{ name: "description", content: "My amazing site." }],
+});
+const projects = ref([
+  {
+    img: "minagoo-thumbnail.png",
+    title: "Minagoo",
+    text: "Minagoo is an online English learning platform to Improve your language skills and it can be used for any age, level, and nationality. ",
+    link: "minagoo.ir",
+    linkName: "minagoo.ir",
+    frontTech: ["HTML-CSS", "JavaScript", "VUE", "NUXT"],
+    date: "Des 6, 2023",
+  },
+  {
+    img: "flylounge-menu.png",
+    title: "Digital menu service",
+    text: " A restaurant cafe menu with a beautiful design and interesting features such as table reservation, choosing food from the menu ",
+    link: " http://menuly.vip/flylounge",
+    linkName: "menuly.vip/flylounge",
+    frontTech: ["HTML-CSS", "JavaScript", "VUE", "NUXT"],
+    date: "Des 6, 2023",
+  },
+  {
+    img: "shahrdari-main.png",
+    title: "137",
+    text: "  A system for municipalities with the possibility of  registering citizens' reports in the city and following up on reports and solving urban problems by managers ",
+    link: "http://kermanshah.137.demo.hemend.com/en",
+    linkName: "http://kermanshah.137.demo.hemend.com/en",
+    frontTech: ["HTML-CSS", "JavaScript", "VUE", "NUXT"],
+    date: "Des 6, 2023",
+  },
+  {
+    img: "karkia.jpg",
+    title: "Karkia",
+    text: "  The Karkia Mattress website is designed to streamline the  mattress shopping experience by offering a robust platform    for browsing, learning about, and purchasing high-quality mattresses. It combines detailed product information,  customer support, and e-commerce functionality to meet theneeds of modern consumers.",
+    link: "http://karkiamattress.com",
+    linkName: "Karkia",
+    frontTech: ["HTML-CSS", "JavaScript"],
+    date: "July 20, 2024",
+  },
+  {
+    img: "abdevali.png",
+    title: "Abdevali Jewelry",
+    text: " The Abdevali Jewelry website serves a dual purpose as both a professional portfolio and an e-commerce platform. The  shop features product listings with descriptions, prices,  and images. Allows users to add products to a shopping  cart and proceed through a secure checkout process to  complete their purchases.",
+    link: "abdevali-tala.vercel.app",
+    linkName: "Abdevali Jewelry",
+    frontTech: ["HTML-CSS", "JavaScript" , "VUE", "NUXT"],
+    date: "July 20, 2024",
+  },
+]);
 </script>
 
 <template>
@@ -37,174 +61,12 @@ const shahrdariInfo = [
       <div class="">
         <v-container>
           <v-row>
-            <v-col cols="12" lg="4" style="--stagger: 1" data-animate>
-              <div type="button">
-                <v-card class="mx-auto projects-card" max-width="374">
-                <template v-slot:loader="{ isActive }">
-                  <v-progress-linear  
-                    :active="isActive"
-                    color="deep-purple"
-                    height="4"
-                    indeterminate></v-progress-linear>
-                </template>
-
-                <v-img cover height="250" src="/assets/images/minagoo-thumbnail.png"></v-img>
-
-                <v-card-item>
-                  <v-card-title>Minagoo</v-card-title>
-                </v-card-item>
-
-                <v-card-text>
-                  <div>
-                    Minagoo is an online English learning platform to Improve your language skills and it can be used for any age, level, and nationality.
-                    <p>
-                      Front-end technologies:
-                    </p>
-                    <ul class="mx-3 my-3">
-                      <li style="font-size: 12px;">
-                        HTML-CSS
-                      </li>
-                      <li style="font-size: 12px;">
-                        JavaScript
-                      </li>
-                      <li style="font-size: 12px;">
-                        VUE
-                      </li>
-                      <li style="font-size: 12px;">
-                        NUXT
-                      </li>
-                    </ul>
-                  </div>
-                  <div>
-                    Minagoo.ir
-                  </div>
-                </v-card-text>
-                <v-card-item>
-                  <v-card-subtitle>
-                   <div>
-                    <span class="me-1">Des 6, 2023</span>
-                   </div>
-                  </v-card-subtitle>
-                </v-card-item>
-              </v-card>
-              </div>
-              
-            </v-col>  
-              <v-col cols="12" lg="4" style="--stagger: 1" data-animate>
-              <div type="button">
-                <v-card class="mx-auto projects-card" max-width="374">
-                <template v-slot:loader="{ isActive }">
-                  <v-progress-linear  
-                    :active="isActive"
-                    color="deep-purple"
-                    height="4"
-                    indeterminate></v-progress-linear>
-                </template>
-
-                <v-img cover height="250" src="/assets/images/flylounge-menu.png"></v-img>
-
-                <v-card-item>
-                  <v-card-title>Flylounge</v-card-title>
-                </v-card-item>
-
-                <v-card-text>
-                  <div>
-                   A restaurant cafe menu with a beautiful design and interesting features such as table reservation, choosing food from the menu
-                    <p>
-                      Front-end technologies:
-                    </p>
-                    <ul class="mx-3 my-3">
-                      <li style="font-size: 12px;">
-                        HTML-CSS
-                      </li>
-                      <li style="font-size: 12px;">
-                        JavaScript
-                      </li>
-                      <li style="font-size: 12px;">
-                        VUE
-                      </li>
-                      <li style="font-size: 12px;">
-                        NUXT
-                      </li>
-                    </ul>
-                  </div>
-                  <div>
-                    <div class="text-red">CHECK IN RESPONSIVE MODE</div>
-                    menuly.vip/flylounge
-                  </div>
-                </v-card-text>
-                <v-card-item>
-                  <v-card-subtitle>
-                   <div>
-                    <span class="me-1">Des 6, 2023</span>
-                   </div>
-                  </v-card-subtitle>
-                </v-card-item>
-              </v-card>
-              </div>
-              
-            </v-col> 
-             <v-col cols="12" lg="4" style="--stagger: 1" data-animate>
-              <div type="button">
-                <v-card class="mx-auto projects-card" max-width="374">
-                <template v-slot:loader="{ isActive }">
-                  <v-progress-linear  
-                    :active="isActive"
-                    color="deep-purple"
-                    height="4"
-                    indeterminate></v-progress-linear>
-                </template>
-
-                <v-img cover height="250" src="/assets/images/shahrdari-main.png"></v-img>
-
-                <v-card-item>
-                  <v-card-title>137</v-card-title>
-                </v-card-item>
-
-                <v-card-text>
-                  <div>
-                    A system for municipalities with the possibility of registering citizens' reports in the city and following up on reports and solving urban problems by managers
-                    <p>
-                      Front-end technologies:
-                    </p>
-                    <ul class="mx-3 my-3">
-                      <li style="font-size: 12px;">
-                        HTML-CSS
-                      </li>
-                      <li style="font-size: 12px;">
-                        JavaScript
-                      </li>
-                      <li style="font-size: 12px;">
-                        VUE
-                      </li>
-                      <li style="font-size: 12px;">
-                        NUXT
-                      </li>
-                    </ul>
-                  </div>
-                  <div>
-                    http://kermanshah.137.demo.hemend.com/en
-                  </div>
-                </v-card-text>
-                <v-card-item>
-                  <v-card-subtitle>
-                   <div>
-                    <span class="me-1">Des 6, 2023</span>
-                   </div>
-                  </v-card-subtitle>
-                </v-card-item>
-              </v-card>
-              </div>
-              
-            </v-col> 
+            <v-col cols="12" sm="6" lg="4" v-for="(item, i) in projects" :key="i">
+              <BaseCard :items="item" :style="['--stagger:' + (i + 1)]" data-animate />
+            </v-col>
           </v-row>
         </v-container>
       </div>
     </article>
-    <ModalBaseModal v-model="minagoo" :item="minagooInfo" />
-    <ModalBaseModal v-model="flylounge" :item="flyInfo" />
-    <ModalBaseModal v-model="shahrdari" :item="shahrdariInfo" />
-
-
   </div>
 </template>
